69e287a24e17b37e485576c138db3b4be2b93dc7
Wenszel <wiktor.smaga13@gmail.com>
2025-05-04 13:40:29 +0200

    feat: added mocked eventService

---
+++ src/services/eventService.js
@@ -0,0 +1,7 @@
+export function getVenues() {
+    return [
+        { id: 1, name: 'Stadion Narodowy', location: 'Warszawa' },
+        { id: 2, name: 'Stadion Wrocław', location: 'Wrocław' },
+        { id: 3, name: 'Stadion Gdańsk', location: 'Gdańsk' },
+    ];
+}
