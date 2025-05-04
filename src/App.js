a4034ea99618c8f1d461073b1e8a4bef1e5ae68f
Wenszel <wiktor.smaga13@gmail.com>
2025-05-04 10:53:30 +0200

    feat: added routing in App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import AcceptReferees from './pages/AcceptReferees';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path="/referees" element={<AcceptReferees />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
